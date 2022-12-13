09a4142c4337157223452b37146e1f0bc2fe1141
Claudia Lozano <cflc@mit.edu>
2022-12-12 23:37:06 -0500

    creating sequences function

--- create_decks.py
+++ create_decks.py
@@ -1,3 +1,8 @@
+""" Creates the Decks based on our different hypothesis and  allows us to get a sequence that is representative
+of that hypotheis"""
+
+from operator import itemgetter
+
 SUITS = ["Diamonds", "Spades", "Hearts", "Clubs"]
 RANKS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
@@ -38,10 +43,36 @@ def create_suit_deck():
     return list(sorted(deck, key=lambda i: sort_order.index(i[1])))
 
 
-#def main():
-    # ascending_deck = create_suit_deck()
-    # print(ascending_deck)
-    # random_deck = create_random_deck()
-    # seq1 = []
-    # for _ in range(5):
-    #     seq1.append(random_deck.pop())
+def create_sequence(h, n):
+    """
+    Input:
+    :param h: integer corresponding to the desired hypothesis
+    :param n: length of sequence
+
+    Output
+    :return: a sequence drawn from a deck corresponding to each sequence
+    """
+
+    if h == 1:
+        deck = create_random_deck()
+    elif h == 2:
+        deck = create_ascending_deck()
+    elif h == 3:
+        deck = create_suit_deck()
+    else:
+        raise Exception("Not a valid hypothesis")
+
+    sequence = []
+    for _ in range(n):
+        sequence.append(deck.pop())
+
+    return sequence
+
+
+# def main():
+# ascending_deck = create_suit_deck()
+# print(ascending_deck)
+# random_deck = create_random_deck()
+# seq1 = []
+# for _ in range(5):
+#     seq1.append(random_deck.pop())
